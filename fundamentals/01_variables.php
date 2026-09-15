a5bb9afb32dec716e295f2869626f567dabded73
tanbl123 <timbunleong1@gmail.com>
2026-09-15 09:46:09 +0800

    version1

---
+++ fundamentals/01_variables.php
@@ -0,0 +1,34 @@
+$name = "John Doe";
+$age = 30;
+$height = 5.9;
+$learningPHP = true;
+
+echo "Name: " .$name. "\n";
+echo "Age: " .$age. "\n";
+echo "Height: " .$height. "\n";
+if ($learningPHP){
+    echo "I am Learning PHP!";
+}else{
+    echo "I am not Learning PHP!";
+}
+
+/*  
+echo "Name: $name\n";
+echo "Age: $age\n";
+echo "Height: $height\n";
+if($learningPHP){
+    echo "I am Learning PHP!";
+} else {
+    echo "I am not Learning PHP!";
+}
+*/
+
+echo 'Name: $name\n';
+echo 'Age: $age\n';
+echo 'Height: $height\n';
+if($learningPHP){
+    echo 'I am Learning PHP!';
+} else {
+    echo 'I am not Learning PHP!';
+}
+
